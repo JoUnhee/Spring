f2d418b769661cf68b89789e2c732220c42ac7f8
JoUnhee <jounhee0107@gmail.com>
2021-07-28 09:12:56 +0900

    - updated test for xml config - updated test to check bean definition

---
+++ basicproject/src/test/java/spring/basicproject/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,40 @@
+package spring.basicproject.beandefinition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import spring.basicproject.AppConfig;
+
+
+public class BeanDefinitionTest {
+
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인 - class")
+    void findApplicationBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+    @Test
+    @DisplayName("빈 설정 메타정보 확인 - xml")
+    void findApplicationBeanXml() {
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
