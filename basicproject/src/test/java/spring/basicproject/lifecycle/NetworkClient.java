089e68cd717b01b9a70fc2e42cf98227dbc71f0b
jounhee <jounhee0107@gmail.com>
2021-08-17 14:14:23 +0900

    - learn callback supported by spring framework - annotation - pre/post ,, - etc

--- basicproject/src/test/java/spring/basicproject/lifecycle/NetworkClient.java
+++ basicproject/src/test/java/spring/basicproject/lifecycle/NetworkClient.java
@@ -1,13 +1,15 @@
 package spring.basicproject.lifecycle;
 
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -28,4 +30,16 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
+    @PostConstruct
+    public void init(){
+        /* 빈의 초기화 시점에 호출되는 함수 */
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    public void close(){
+        /* 빈의 생명주기중 빈이 종료될 때 호출되는 함수 */
+        disconnect();
+    }
 }
